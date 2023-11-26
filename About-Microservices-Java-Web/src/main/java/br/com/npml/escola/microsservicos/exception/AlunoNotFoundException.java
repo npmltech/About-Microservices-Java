8b030f2bf338d8e42220da84c959a870213a1793
npalleta <palleta.sp@gmail.com>
2023-11-26 15:34:37 -0300

    added AlunoException

--- About-Microservices-Java-Web/src/main/java/br/com/npml/escola/microsservicos/exception/AlunoNotFoundException.java
+++ About-Microservices-Java-Web/src/main/java/br/com/npml/escola/microsservicos/exception/AlunoNotFoundException.java
@@ -1,2 +1,19 @@
-package br.com.npml.escola.microsservicos.exception;public class AlunoNotFoundException {
+package br.com.npml.escola.microsservicos.exception;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlunoNotFoundException extends RuntimeException {
+
+    private String errorMessage;
+
+    private Throwable err;
+
+    private LocalDateTime localDateTime;
 }
