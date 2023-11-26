af81b89d8d9d5543a26907a5d39055f30e4f7e39
npalleta <palleta.sp@gmail.com>
2023-11-26 15:46:34 -0300

    AlunoRepository has been included

---
+++ About-Microservices-Java-Web/src/main/java/br/com/npml/escola/microsservicos/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package br.com.npml.escola.microsservicos.repository;
+
+import br.com.npml.escola.microsservicos.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
