88f8885175b0e460b86ac360a4bb25f03ec2e3f5
npalleta <palleta.sp@gmail.com>
2023-11-26 15:32:17 -0300

    added application class

--- About-Microservices-Java-Web/src/main/java/br/com/npml/escola/microsservicos/EscolaBackendApplication.java
+++ About-Microservices-Java-Web/src/main/java/br/com/npml/escola/microsservicos/EscolaBackendApplication.java
@@ -1,4 +1,4 @@
-package br.com.npml;
+package br.com.npml.escola.microsservicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
