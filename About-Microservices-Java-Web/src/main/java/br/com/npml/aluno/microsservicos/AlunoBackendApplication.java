2cb481a7371665677eabd55d42e5afcfa4f0bc21
npalleta <palleta.sp@gmail.com>
2023-12-03 20:08:44 -0300

    pom.xml has been updated

--- About-Microservices-Java-Web/src/main/java/br/com/npml/aluno/microsservicos/AlunoBackendApplication.java
+++ About-Microservices-Java-Web/src/main/java/br/com/npml/aluno/microsservicos/AlunoBackendApplication.java
@@ -1,4 +1,4 @@
-package br.com.npml.escola.microsservicos;
+package br.com.npml.aluno.microsservicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- About-Microservices-Java-Web/src/test/java/br/com/npml/aluno/microsservicos/AlunoBackendApplicationTest.java
+++ About-Microservices-Java-Web/src/test/java/br/com/npml/aluno/microsservicos/AlunoBackendApplicationTest.java
@@ -1,4 +1,4 @@
-package br.com.npml.escola.microsservicos;
+package br.com.npml.aluno.microsservicos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
